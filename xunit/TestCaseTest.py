7e73a39bf319eb203ec7cf94a1f526e5f87f46f2
lsw1164 <qkqkfo@gmail.com>
2020-01-05 15:54:16 +0900

    Create TestResult class
     - Consider multiple results

--- xunit/TestCaseTest.py
+++ xunit/TestCaseTest.py
@@ -9,3 +9,8 @@ class TestCaseTest(TestCase):
         test=WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+
+    def testFailedResult(self):
+        test=WasRun("testBrokenMethod")
+        result=test.run()
+        assert("1 run, 1 failed" == result.summary())
